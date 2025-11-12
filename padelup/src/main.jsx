0c1a8b0acce5a0ca894c675a423e981d77bd3cec
bertiny11 <alberto.herediatorrejon@alum.uca.es>
2025-11-12 09:19:44 +0100

    Actualizado (Funcionamiento Correcto)

--- padelup/src/main.jsx
+++ padelup/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Register from './pages/register.jsx';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Register />
   </StrictMode>,
 )
