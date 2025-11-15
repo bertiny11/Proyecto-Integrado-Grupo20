7704b80c872741725c06d4d10ad9647eaf8db49a
Pablo <PaabloRB7@users.noreply.github.com>
2025-11-15 16:11:41 +0100

    Cambio main jsx
    Ahora muestra el home

--- padelup/src/main.jsx
+++ padelup/src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Register from './pages/register.jsx';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Register  />
+    <App />
   </StrictMode>,
 )
